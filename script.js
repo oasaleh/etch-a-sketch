721e58b835b5aa660c0806801929013fa450be9a
Omar Saleh <omarsaleh92@gmail.com>
2020-10-09 10:51:37 -0500

    Fixed JS calls

--- script.js
+++ script.js
@@ -8,8 +8,20 @@ function makeGrid(rows, cols) {
     // This create the HTML div elements needed.
     for (i = 1; i <= (rows * cols); i++) {
         let eachCell = document.createElement('div');
-        eachCell.innerHTML = i;
+        // eachCell.innerHTML = i;
         container.appendChild(eachCell).className = 'grid-cell';
     }
 }
-makeGrid(16, 16);
+makeGrid(60, 120);
+
+// This following statement does nto work because getElementsByClassName returns an
+// array. And this array (aka an object) does not have addEventListener method.
+// document.getElementsByClassName('grid-cell').addEventListener('mouseover', changeColor)
+
+let cells = document.getElementsByClassName('grid-cell');
+for (let cell of cells) {
+    cell.addEventListener('mouseover', changeColor)
+}
+function changeColor(e) {
+    console.log(e)
+}
